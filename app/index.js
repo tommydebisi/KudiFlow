b347bb674f5b9518c5d1250112e2ac65e3f6de7c
tommydebisi <tommydebisi@gmail.com>
2023-03-05 16:55:21 +0100

    :seedling: semi-colons

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // load environment variables
-require('dotenv').config()
+require('dotenv').config();
 
 const userName = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
@@ -9,7 +9,7 @@ const db = process.env.DB_NAME;
 async function connectDb() {
   await mongoose.connect(
     `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ijtgnu3.mongodb.net/${process.env.DB_NAME}`
-  )
+  );
 }
 
 module.exports = connectDb;
--- app/tests/test.js
+++ app/tests/test.js
@@ -11,8 +11,8 @@ connectDb().then(() => {
 async function start() {
   try{
     const user = await User.where('email').equals('tommy@foo.com').limit(1);
-    user[0].track = '6404b7c8ecd5a4b7a6d5fbde'
-    user[0].save()
+    user[0].track = '6404b7c8ecd5a4b7a6d5fbde';
+    user[0].save();
     console.log(user);
   } catch(e) {
     console.log(e.message);
