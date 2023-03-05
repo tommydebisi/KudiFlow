b2b2aadeb960032eb4319cf7168da68e126d34df
tommydebisi <tommydebisi@gmail.com>
2023-03-05 16:49:19 +0100

    :construction: db connector

---
+++ app/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+// load environment variables
+require('dotenv').config()
+
+const userName = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const db = process.env.DB_NAME;
+
+async function connectDb() {
+  await mongoose.connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ijtgnu3.mongodb.net/${process.env.DB_NAME}`
+  )
+}
+
+module.exports = connectDb;
