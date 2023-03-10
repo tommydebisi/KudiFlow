3afcbb1ff4aa14250ab36ec5dd898ade671cd97f
tommydebisi <tommydebisi@gmail.com>
2023-03-10 14:01:19 +0100

    :seedling: databases

---
+++ app/utils/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+// load environment variables
+require('dotenv').config();
+
+class DBClient {
+  constructor() {
+      this._username = process.env.DB_USERNAME;
+      this._password = process.env.DB_PASSWORD;
+      this._db = process.env.DB_NAME;
+
+      mongoose.connect(
+        `mongodb+srv://${this._username}:${this._password}@cluster0.ijtgnu3.mongodb.net/${this._db}`
+      );
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
