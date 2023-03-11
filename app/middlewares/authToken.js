841216474b12d6d59750ae2a97a7a3ac97621032
tommydebisi <tommydebisi@gmail.com>
2023-03-11 07:01:54 +0100

    :seedling: app middleware to verify authorization

---
+++ app/middlewares/authToken.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+function authToken(req, res, next) {
+  const auth = req.get('Authorization');
+
+  // format: Bearer Token (get the token from the auth header)
+  const token = auth && auth.split(' ')[1];
+  if (!token) return res.status(400).json({ error: 'Missing Token' });
+
+  // verify the token gotten
+  jwt.verify(token, process.env.API_SECRET, (err, field) => {
+    if (err) return res.status(401).json({ error: 'Unauthorized' });
+
+    // set request var and move to next middleware
+    req.email = field.email;
+    req.token = token;
+    next();
+  });
+}
+module.exports = authToken;
